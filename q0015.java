3a28ed43df0bb88a2444c048b6c92b8d12f7509a
Praduman sharma <54066940+praduman99@users.noreply.github.com>
2019-11-29 17:35:26 +0530

    q0015

--- q0015.java
+++ q0015.java
@@ -1,48 +1,59 @@
-
 import java.util.*;
- 
- static class Node
-	{
-		Node left,right;
-		int data;
-		Node(int data)
-		{
-			this.data=data;
-			left=null;
-			right=null;
-		}
-	}
-	static Node root;
-	Node findl(int a,int b)
-	{
-		return lowest(root,a,b);
-	}
-	Node lowest(Node root,int a,int b)
-	{
-		if(root==null)
-			return root;
-		if(root.data==a||root.data==b)
-			return root;
-		Node l=lowest(root.left,a,b);
-		Node r=lowest(root.right,a,b);
-		if(l!=null&&r!=null)
-			return root;
-		return (l!=null)?l:r;
-	}
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		lca c=new lca();
-		c.root=new Node(1);
-		c.root.left=new Node(2);
-		c.root.right=new Node(3);
-		c.root.left.left=new Node(4);
-		c.root.left.right=new Node(5);
-		c.root.right.left=new Node(6);
-		c.root.right.right=new Node(7);
-		System.out.println("Enter the two nodes");
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		Node t=c.lowest(c.root,a,b);
-		System.out.print(t.data+" ");
-	}
+class LeftView
+{
+    public static Node root;
+    static int prev_lev=0;
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data=data;
+        }
+    }
+    public static int height(Node n1)
+    {
+        if(n1==null)
+            return 0;
+        return 1+Math.max(height(n1.left),height(n1.right));
+    }
+    public static  void leftView(Node root)
+    {
+        int h=height(root);
+        for(int i=1;i<=h;i++)
+        {
+            prev_lev=1;
+            printLeftView(root,i);
+        }
+    }
+    public static void printLeftView(Node root,int level)
+    {
+        if(root==null)
+            return;
+        if(level==1 && prev_lev==1)
+        {
+            System.out.println(" "+root.data);
+            prev_lev++;
+        }
+        else
+        {
+            printLeftView(root.left, level-1);
+            printLeftView(root.right, level-1);
+        }
+
+    }
+    public static void main(String[] args) {
+        LeftView t1=new LeftView();
+        t1.root=new Node(1);
+        t1.root.left=new Node(2);
+        t1.root.right=new Node(3);
+        t1.root.left.left=new Node(4);
+        t1.root.left.right=new Node(5);
+        t1.root.right.right=new Node(6);
+        t1.root.left.right.left=new Node(7);  
+        t1.root.left.right.left.right=new Node(8); 
+        leftView(t1.root);
+    }
+}
